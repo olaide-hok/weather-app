b104b22cc509fc19a6f12194ebc05b9a5d997e4c
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-13 18:50:50 +0100

    move state to parent and then recieve as props

--- components/days-dropdown.tsx
+++ components/days-dropdown.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 const days = [
   "Monday",
   "Tuesday",
@@ -12,16 +10,19 @@ const days = [
   "Sunday",
 ];
 
-const DaysDropdown = () => {
-  const [selectedDay, setSelectedDay] = useState("Monday");
+interface DaysDropdownProps {
+  selectedDay: string;
+  selectDay: (day: string) => void;
+}
 
+const DaysDropdown = ({ selectedDay, selectDay }: DaysDropdownProps) => {
   return (
     <div className="flex flex-col gap-y-(--sp-050) rounded-(--radius-12) border border-(--clr-neutral-600) bg-(--clr-neutral-800) p-(--sp-100) shadow-[0px_8px_16px_0px_rgba(2,1,44,0.32)] lg:w-[13.375rem]">
       {days.map((day, index) => (
         <p
           key={index}
-          className={`cursor-pointer ${day === selectedDay ? "rounded-(--radius-8) bg-(--clr-neutral-700)" : ""} px-(--sp-100) py-(--sp-125) text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0)`}
-          onClick={() => setSelectedDay(day)}
+          className={`cursor-pointer ${day === selectedDay ? "rounded-(--radius-8) bg-(--clr-neutral-700)" : ""} px-(--sp-100) py-(--sp-125) text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0) hover:bg-(--clr-neutral-700)`}
+          onClick={() => selectDay(day)}
         >
           {day}
         </p>
