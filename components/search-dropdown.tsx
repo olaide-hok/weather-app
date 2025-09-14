8adfd53beaef838b97559ddbeabd342e57297939
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-14 17:36:53 +0100

    update component to recieve suggestions and onSelect props

--- components/search-dropdown.tsx
+++ components/search-dropdown.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import { useState } from "react";
+interface Suggestion {
+  name: string;
+  latitude: number;
+  longitude: number;
+  country: string;
+}
 
-const searchHistory = ["City name", "City name", "City name", "City name"];
+interface SearchDropdownProps {
+  suggestions: Suggestion[];
+  onSelect: (suggestion: Suggestion) => void;
+}
 
-const SearchDropdown = () => {
-  const [active, setActive] = useState(0);
+const SearchDropdown = ({ suggestions, onSelect }: SearchDropdownProps) => {
   return (
-    <div className="flex flex-col gap-y-(--sp-050) rounded-(--radius-12) border border-(--clr-neutral-700) bg-(--clr-neutral-800) p-(--sp-100) lg:w-[32.875rem]">
-      {searchHistory.map((history, index) => (
+    <div className="absolute top-[4rem] z-10 flex max-h-[11.5rem] flex-col gap-y-(--sp-050) overflow-y-auto rounded-(--radius-12) border border-(--clr-neutral-700) bg-(--clr-neutral-800) p-(--sp-100) lg:w-[32.875rem]">
+      {suggestions.map((suggestion, index) => (
         <span
           key={index}
-          className={`cursor-pointer ${active === index ? "rounded-(--radius-8) border border-(--clr-neutral-600) bg-(--clr-neutral-700)" : ""} px-(--sp-100) py-(--sp-125) text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0)`}
-          onClick={() => setActive(index)}
+          className={`cursor-pointer px-(--sp-100) py-(--sp-125) text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0) hover:rounded-(--radius-8) hover:border hover:border-(--clr-neutral-600) hover:bg-(--clr-neutral-700)`}
+          onClick={() => onSelect(suggestion)}
         >
-          {history}
+          {suggestion.name}, {suggestion.country}
         </span>
       ))}
     </div>
