1ec34e932171d476ba9e7bfc2591365cf93234e4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-14 07:39:16 +0100

    setup hero-loading skeleton

---
+++ components/hero-loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/skeleton";
+import { Icons } from "./icons";
+
+export const HeroLoading = () => {
+  return (
+    <Skeleton className="flex w-full flex-col items-center justify-center gap-y-(--sp-200) rounded-(--radius-12) bg-(--clr-neutral-800) md:h-[17.875rem]">
+      <Icons.loading />
+      <p className="text-(length:--fs-18) leading-(--lh-120) font-medium text-(--clr-neutral-200)">
+        Loading...
+      </p>
+    </Skeleton>
+  );
+};
