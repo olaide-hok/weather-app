9d400cd9b13706352107a160dbd9c71a02a10718
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-17 17:27:23 +0100

    update class name based on loading and value prop values.

--- components/thwp-container.tsx
+++ components/thwp-container.tsx
@@ -20,14 +20,16 @@ interface THWPContainerProps {
  */
 const THWPContainer = ({ title, value, loading, unit }: THWPContainerProps) => {
   return (
-    <div className="flex flex-col gap-y-(--sp-300) rounded-(--radius-12) border border-(--clr-neutral-600) bg-(--clr-neutral-800) p-(--sp-250)">
+    <div
+      className={`${loading && !!value ? "animate-pulse" : ""} flex flex-col gap-y-(--sp-300) rounded-(--radius-12) border border-(--clr-neutral-600) bg-(--clr-neutral-800) p-(--sp-250)`}
+    >
       <span className="text-(length:--fs-18) leading-(--lh-120) font-medium text-(--clr-neutral-200)">
         {title}
       </span>
       <span
-        className={`${loading ? "animate-pulse" : ""} text-(length:--fs-32) font-light text-(--clr-neutral-0)`}
+        className={`${loading && !!value ? "animate-pulse" : ""} text-(length:--fs-32) font-light text-(--clr-neutral-0)`}
       >
-        {loading ? "-" : `${value} ${unit}`}
+        {loading && !!value ? "-" : `${value} ${unit}`}
       </span>
     </div>
   );
