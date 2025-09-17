36e5c5c9d30a3a5124aaaf762b7ec3770aa304a0
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-17 18:07:52 +0100

    rmeove loading prop and cleanup jsx

--- components/thwp-container.tsx
+++ components/thwp-container.tsx
@@ -3,7 +3,6 @@
 interface THWPContainerProps {
   title: string;
   value: string;
-  loading: boolean;
   unit: string;
 }
 
@@ -13,23 +12,18 @@ interface THWPContainerProps {
  *
  * @param {string} title - The title of the weather condition.
  * @param {string} value - The value of the weather condition.
- * @param {string} loading - The loading state.
  * @param {string} unit - The weather unit.
  *
  * @returns {JSX.Element} A JSX element displaying the title and the value.
  */
-const THWPContainer = ({ title, value, loading, unit }: THWPContainerProps) => {
+const THWPContainer = ({ title, value, unit }: THWPContainerProps) => {
   return (
-    <div
-      className={`${loading && !!value ? "animate-pulse" : ""} flex flex-col gap-y-(--sp-300) rounded-(--radius-12) border border-(--clr-neutral-600) bg-(--clr-neutral-800) p-(--sp-250)`}
-    >
+    <div className="flex flex-col gap-y-(--sp-300) rounded-(--radius-12) border border-(--clr-neutral-600) bg-(--clr-neutral-800) p-(--sp-250)">
       <span className="text-(length:--fs-18) leading-(--lh-120) font-medium text-(--clr-neutral-200)">
         {title}
       </span>
-      <span
-        className={`${loading && !!value ? "animate-pulse" : ""} text-(length:--fs-32) font-light text-(--clr-neutral-0)`}
-      >
-        {loading && !!value ? "-" : `${value} ${unit}`}
+      <span className="text-(length:--fs-32) font-light text-(--clr-neutral-0)">
+        {`${value} ${unit}`}
       </span>
     </div>
   );
