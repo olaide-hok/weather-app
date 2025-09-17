b6bfa29b27fdef00cb2bcfd6d72faab0bf3a9079
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-17 18:02:03 +0100

    update utility classes

--- components/hero-loading.tsx
+++ components/hero-loading.tsx
@@ -3,7 +3,7 @@ import { Icons } from "./icons";
 
 export const HeroLoading = () => {
   return (
-    <Skeleton className="flex w-full flex-col items-center justify-center gap-y-(--sp-200) rounded-(--radius-12) bg-(--clr-neutral-800) md:h-[17.875rem]">
+    <Skeleton className="flex h-[17.875rem] w-full flex-col items-center justify-center gap-y-(--sp-200) rounded-(--radius-12) bg-(--clr-neutral-800)">
       <Icons.loading />
       <p className="text-(length:--fs-18) leading-(--lh-120) font-medium text-(--clr-neutral-200)">
         Loading...
