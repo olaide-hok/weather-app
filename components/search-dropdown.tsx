a0bf86f44122f110de5dee03b08b776b71f1340d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-21 12:25:42 +0100

    update component to show country flag alongside its name

--- components/search-dropdown.tsx
+++ components/search-dropdown.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { CircleFlag } from "react-circle-flags";
+
 interface Suggestion {
   name: string;
   latitude: number;
   longitude: number;
   country: string;
+  country_code: string;
 }
 
 interface SearchDropdownProps {
@@ -16,13 +19,21 @@ const SearchDropdown = ({ suggestions, onSelect }: SearchDropdownProps) => {
   return (
     <div className="scrollbar absolute top-[4rem] z-10 flex max-h-[11.5rem] flex-col gap-y-(--sp-050) overflow-y-auto rounded-(--radius-12) border border-(--clr-neutral-700) bg-(--clr-neutral-800) p-(--sp-100) lg:w-[32.875rem]">
       {suggestions.map((suggestion, index) => (
-        <span
+        <div
           key={index}
-          className={`cursor-pointer px-(--sp-100) py-(--sp-125) text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0) hover:rounded-(--radius-8) hover:border hover:border-(--clr-neutral-600) hover:bg-(--clr-neutral-700)`}
-          onClick={() => onSelect(suggestion)}
+          className="flex cursor-pointer items-center gap-x-(--sp-100) px-(--sp-100) py-(--sp-125) hover:rounded-(--radius-8) hover:border hover:border-(--clr-neutral-600) hover:bg-(--clr-neutral-700)"
         >
-          {suggestion.name}, {suggestion.country}
-        </span>
+          <CircleFlag
+            countryCode={suggestion.country_code.toLowerCase()}
+            className="h-[1.5rem] w-[1.5rem]"
+          />
+          <span
+            className={`text-(length:--fs-16) leading-(--lh-120) font-(familiy-name:--font-dm-sans) text-(--clr-neutral-0)`}
+            onClick={() => onSelect(suggestion)}
+          >
+            {suggestion.name}, {suggestion.country}
+          </span>
+        </div>
       ))}
     </div>
   );
