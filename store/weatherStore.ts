23865df1b7e7706bb8899d244778d1526cf89573
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-09-21 17:57:27 +0100

    update state values for lat, long, and cityName

--- store/weatherStore.ts
+++ store/weatherStore.ts
@@ -6,10 +6,10 @@ import {
 } from "@/lib/utils";
 
 // Berlin, Germany coordinates for initial state
-const berlinGermanyCoordinates = {
-  lat: 52.52000045776367,
-  long: 13.419998168945312,
-};
+// const berlinGermanyCoordinates = {
+//   lat: 52.52000045776367,
+//   long: 13.419998168945312,
+// };
 
 interface CurrentWeatherData {
   time: string;
@@ -46,8 +46,8 @@ interface WeatherStoreState {
   currentWeatherData: CurrentWeatherData | null;
   dailyForecastData: DailyForecast[] | null;
   hourlyForecastData: HourlyForecastDataPerDay[] | null;
-  lat: number;
-  long: number;
+  lat: number | null;
+  long: number | null;
   loading: boolean;
   error: boolean;
   noResultFound: boolean;
@@ -71,12 +71,12 @@ export const useWeatherStore = create<WeatherStoreState>((set, get) => ({
     await get().fetchDailyForecastData();
     await get().fetchHourlyForecastData();
   },
-  cityName: "Berlin, Germany",
+  cityName: "Loading...",
   currentWeatherData: null,
   dailyForecastData: null,
   hourlyForecastData: null,
-  lat: berlinGermanyCoordinates.lat,
-  long: berlinGermanyCoordinates.long,
+  lat: null,
+  long: null,
   loading: false,
   error: false,
   noResultFound: false,
